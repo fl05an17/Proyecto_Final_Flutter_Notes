8c15ebe89f063ea817e9ed9e5e68d491b316a0b0
Santiago Florez L <31561075+fl05an17@users.noreply.github.com>
2022-11-05 11:28:37 -0500

    Feat: Se agrega CRUD final
    Se agregan screens finales y navegacion entre pantallas, adicional a el CRUD donde podemos Crear, Y leer notas.

---
+++ lib/aplicacion/note_editor.dart
@@ -0,0 +1,91 @@
+import 'dart:math';
+
+import 'package:base_tortas/styles/app_style.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class NoteEditorScreen extends StatefulWidget {
+  const NoteEditorScreen({super.key});
+
+  @override
+  State<NoteEditorScreen> createState() => _NoteEditorScreenState();
+}
+
+class _NoteEditorScreenState extends State<NoteEditorScreen> {
+  //Se definenn la variables para manipular los valores de las notas
+  int color_id = Random().nextInt(AppStyle.cardsColor.length);
+  String date = DateTime.now().toString();
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _mainController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    //Se define el widget Scaffold el cual nos maneja la estrucuta y diseño de la screen donde podremos agregar nuevas notas
+    return Scaffold(
+      backgroundColor: AppStyle.cardsColor[color_id],
+      appBar: AppBar(
+        backgroundColor: AppStyle.cardsColor[color_id],
+        elevation: 0.0,
+        iconTheme: IconThemeData(color: Colors.black),
+        title: Text(
+          "Agregar nueva nota",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Titulo de la nota',
+              ),
+              style: AppStyle.mainTitle,
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            Text(
+              date,
+              style: AppStyle.dateTitle,
+            ),
+            SizedBox(
+              height: 28.0,
+            ),
+            TextField(
+              controller: _mainController,
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Contenido de la nota',
+              ),
+              style: AppStyle.mainContent,
+            ),
+          ],
+        ),
+      ),
+      //Boton con las acciones definidas para almacenar la nota en fireStore
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppStyle.accentColor,
+        onPressed: () async {
+          FirebaseFirestore.instance.collection("Notes").add({
+            "note_title": _titleController.text,
+            "creation_date": date,
+            "note_content": _mainController.text,
+            "color_id": color_id
+          }).then((value) {
+            print(value.id);
+            Navigator.pop(context);
+          }).catchError((error) => print(
+              "Lo sentimos no se pudo agregar la nota, Intenta nuevamente. Mensaje de Error$error"));
+        },
+        child: Icon(Icons.save),
+      ),
+    );
+  }
+}
